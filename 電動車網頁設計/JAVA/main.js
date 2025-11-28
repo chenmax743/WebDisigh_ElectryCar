a3a33ad7eef19618b9a471ca08dd41a82277b3ea
chenmax743 <84305789+chenmax743@users.noreply.github.com>
2025-11-28 10:33:08 +0800

    RN004

--- 電動車網頁設計/JAVA/main.js
+++ 電動車網頁設計/JAVA/main.js
@@ -1,5 +1,18 @@
-<script>
-  function myFunction() {
-    console.log('Button clicked via onclick attribute!');
+var myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+let icecream = "chocolate";
+if(icecream === "chocolate")
+  {
+    alert("I love chocolate ice cream!");
   }
-</script>
+  else
+  {
+    alert("Chocolate is no like");
+  }
+  function multiply (num1,num2)
+  {
+    let result = num1 * num2;
+    return result;
+  }
+  let myVariable = querySelector("h1");
+  alert(myVariable);
